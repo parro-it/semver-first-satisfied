a3471eed3f7673d28dddda6cdccd0daf114f3f0b
Andrea Parodi <andrea@parro.it>
2016-01-29 14:57:01 +0100

    removed es6 code

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use strict';
 
-const semver = require('semver');
-const semverMax = require('semver-max');
+var semver = require('semver');
+var semverMax = require('semver-max');
 
 module.exports = function firstSatisfied(range, versions) {
-  const satisfing = versions.filter(ver => semver.satisfies(ver, range));
+  var satisfing = versions.filter(function(ver) {
+    return semver.satisfies(ver, range);
+  });
   if (satisfing.length === 0) {
     return null;
   }
--- test.js
+++ test.js
@@ -1,15 +1,15 @@
-const test = require('tape');
-const firstSatisfied = require('./');
+var test = require('tape');
+var firstSatisfied = require('./');
 
-test('return minimum satisfing version', t => {
-  const result = firstSatisfied('^1.1.0', ['0.5.0', '1.1.4', '1.2.3', '1.1.1']);
+test('return minimum satisfing version', function(t) {
+  var result = firstSatisfied('^1.1.0', ['0.5.0', '1.1.4', '1.2.3', '1.1.1']);
 
   t.equal(result, '1.1.1');
   t.end();
 });
 
-test('return null if no version satisifies range', t => {
-  const result = firstSatisfied('^2.1.0', ['0.5.0', '1.1.4', '1.2.3', '1.1.1']);
+test('return null if no version satisifies range', function(t) {
+  var result = firstSatisfied('^2.1.0', ['0.5.0', '1.1.4', '1.2.3', '1.1.1']);
 
   t.equal(result, null);
   t.end();
